7bdd81509cca85934875a22fdd7dd0ea6e261de7
Nikosane <linux.nikosane@gmail.com>
2025-03-21 22:11:32 +0530

    simulation settings and configuration

---
+++ config.py
@@ -0,0 +1,9 @@
+CONFIG = {
+    'num_species': 5,
+    'simulation_steps': 100,
+    'initial_physics': {
+        'gravity': 9.8,
+        'atmosphere_density': 1.2,
+        'temperature': 300
+    }
+}
