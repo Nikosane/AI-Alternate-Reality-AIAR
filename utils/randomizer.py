e165bdacb336bcb3a1add38b65989a4773cf38c4
Nikosane <linux.nikosane@gmail.com>
2025-03-27 22:58:28 +0530

    generates procedural data

--- utils/randomizer.py
+++ utils/randomizer.py
@@ -11,3 +11,9 @@ class Randomizer:
         adaptability = round(random.uniform(0.5, 2.0), 2)
         population = random.randint(1000, 50000)
         return {"name": name, "intelligence": intelligence, "adaptability": adaptability, "population": population}
+
+    def generate_society(self, species_name):
+        name = random.choice(self.society_names)
+        culture_level = round(random.uniform(1.0, 5.0), 2)
+        military_power = round(random.uniform(0.5, 3.0), 2)
+        return {"name": name, "species_name": species_name, "culture_level": culture_level, "military_power": military_power}
